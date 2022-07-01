c90763e120c8f9655da9f64fb3ee65043020e46a
Sunusi Sani Usman <sunusidogo@gmail.com>
2022-07-01 07:50:01 -0700

    hdhdh

---
+++ 0x00-hello_world/102-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints exactly to the standard error
+ * Return: 1
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
