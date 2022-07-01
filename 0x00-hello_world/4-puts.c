5ec8df4636ae03b6656fd872805d7cb7682d6456
Sunusi Sani Usman <sunusidogo@gmail.com>
2022-06-30 23:56:14 -0700

    hfhf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
- * Printing Programming is like building a multilingual puzzle
- * Using Puts stdio
+ * Main - Entry point
+ * 
+ * Return: Always 0 (Success)
  */
-int main() 
-{
+int main(void) 
+
 	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
-}
+
 
 	
