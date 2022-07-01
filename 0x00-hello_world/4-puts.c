e15e77a4dee23e4b3e91a42b238411be94b7bbb6
Sunusi Sani Usman <sunusidogo@gmail.com>
2022-07-01 00:40:56 -0700

    hdgd

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * Main - program that prints exactly the input
- * 
- * Return: Always 0 (Success)
+ * Main - program that prints exactly \"Pramming is like building a multilingual puuzzle
+ * Return: Always 0
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
