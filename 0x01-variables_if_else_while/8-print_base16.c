117ed105f8fd7d01656e31fad8443d2e544c37b8
Sunusi Sani Usman <sunusidogo@gmail.com>
2022-07-03 09:07:30 -0700

    gdgdf

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print all the numbers in base 16
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int n;
+{
+	for (n = 0; n < 16; n++)
+		putchar(n);
+	putchar('\n');
+}
+	return (0);
+}
