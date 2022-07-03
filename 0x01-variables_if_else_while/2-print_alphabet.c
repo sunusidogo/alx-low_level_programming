542ac5eceb343a6f4a04805af616276fb76ab829
Sunusi Sani Usman <sunusidogo@gmail.com>
2022-07-03 00:12:43 -0700

    hfhf

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,15 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 /**
  * main - prints the alphabets in lowercase, and then UPPERCASE
  *
  * Return: always 0
  */
-void main(void)
+int main(void)
 {
 	char alphabet;
 
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		putchar(alphabet);
 	putchar('\n');
-	return (0);
+	return 0;
 }
