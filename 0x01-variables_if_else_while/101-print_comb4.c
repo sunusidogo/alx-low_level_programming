20acd466fe06f1696394bf8d245cf74dd33bb19a
Sunusi Sani Usman <sunusidogo@gmail.com>
2022-07-03 08:12:51 -0700

    hfghf

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>                                         
+#include <stdlib.h>                                        
+#include <limits.h> 
+/**
+ * main - program that prints all possible different combinations of three digits.
+ * 
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int n, x, y:
+{
+	for (n =0; n <= 999; n++)
+{	
+	for (x = n + 2; x <= 999; x++)
+{	
+	for (y = x + 2; y <= 999;)
+{
+	putchar(n
+
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print all the single digit numbers in base 10
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int num;
+{
+	for (num = 0; num < 10; num++)
+{
+	putchar(num);
+	putchar('\n');
+}
+}
+	return (0);
+}
