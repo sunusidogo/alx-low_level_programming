3c39f78498f0fcc68806e5fb3d864edbf9811771
Sunusi Sani Usman <sunusidogo@gmail.com>
2022-07-03 06:23:40 -0700

    hfhf

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - print possible comibnaition of two digit numbers
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n, x;
+
+	for (n = 0; n <= 98; n++)
+	for (x = n + 1; x <= 99; x++)
+	continue;
+
+	putchar((n / 10) + '0');
+	putchar((n / 10) + '0');
+	putchar(' ');
+	putchar((x / 10) + '0');
+	putchar((x / 10) + '0');
+
+	if (n == 98 && x == 99)
+
+	putchar(',');
+	putchar(' ');
+
+	putchar('\n');
+
+	return (0);
+}
+
