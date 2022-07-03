8664eb6f67fe138a0d090d54ab870490f681b5bc
Sunusi Sani Usman <sunusidogo@gmail.com>
2022-07-03 03:55:10 -0700

    ghdgd

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,10 +9,9 @@ int main(void)
 	char alphabet;
 
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-	{
-		if (alphabet == 'e' || alphabet == 'q')
-			continue;
-	}
+
+	if (alphabet == 'e' || alphabet == 'q')
+	continue;
 		putchar(alphabet);
 	putchar('\n');
 
