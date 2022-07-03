8beef39da572514ef179b447c33e74fc635c07a4
Sunusi Sani Usman <sunusidogo@gmail.com>
2022-07-03 08:55:44 -0700

    ghdfgf

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print alphabets in reverse order
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char ch;
+{
+	for (ch = 'z'; ch >= 'a'; ch--)
+	putchar(ch);
+	putchar('\n');
+
+}
+	return (0);
+}
