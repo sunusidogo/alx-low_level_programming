4f906822cb0c014425d1fdfff3a2f9cfb7c9d139
Sunusi Sani Usman <sunusidogo@gmail.com>
2022-07-04 05:43:56 -0700

    updated

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,12 +9,12 @@ int main(void)
 	int num1;
 	int num2;
 {
-	for (num1 = 0; num1 < 8; num1++)
-	for (num2 = 0; num2 < 9; num2++)
+	for (num1 = 0; num1 < 9; num1++)
+	for (num2 = 1; num2 < 10; num2++)
 {
 	putchar((num1 % 10) + '0');
 	putchar((num2 % 10) + '0');
-	if (num1 == 8 && num2 == 9)
+	if (num1 == 9 && num2 == 10)
 	continue;
 	putchar('.');
 	putchar(' ');
