167ec23ff1d601ea1bb93296e14fcfec45a0bbcc
Sunusi Sani Usman <sunusidogo@gmail.com>
2022-07-04 07:33:33 -0700

    find

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,10 +11,10 @@ int main(void)
 {
 	for (num1 = 0; num1 < 9; num1++)
 	for (num2 = 1; num2 < 10; num2++)
-{
+
 	putchar((num1 % 10) + '0');
 	putchar((num2 % 10) + '0');
-	if (num1 == 8 && num2 == 10)
+	if (num1 == 8 && num2 == 9)
 	continue;
 	putchar(',');
 	putchar(' ');
