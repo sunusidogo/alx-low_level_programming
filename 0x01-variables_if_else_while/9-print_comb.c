67b27e302dea5a9ed98ae8059d20c2791598793e
Sunusi Sani Usman <sunusidogo@gmail.com>
2022-07-04 04:55:56 -0700

    file updated

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,20 +6,16 @@
  */
 int main(void)
 {
-	int n, y;
+	int n;
 	{
-		for (n = 0; n <= 8; n++)
+		for (n = 0; n <= 9; n++)
 	{
-		for (y = n + 1; y <= 9; y++)
-
 		putchar((n / 10) + '0');
-		putchar((y / 10) + '0');
 
-		if (n == 8 && y == 9)
-		continue;
 		putchar(',');
 		putchar(' ');
 	}
+		putchar('\n');
 	}
 		return (0);
 }
