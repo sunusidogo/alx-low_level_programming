1f2843f640d6d6fb871dc2ce12a6d52305a8564a
Sunusi Sani Usman <sunusidogo@gmail.com>
2022-07-06 03:00:36 -0700

    updated

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - calls other function
+ *
+ * Funcion: always 0
+ */
+int main(void)
+{
+	_putchar();
+}
+	return (0); 
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,9 +2,9 @@
 #define MAIN_H
 
 int _putchar(char c);
-void print_putchar(void)
-void print_print_alphabet_x10(void)
-void print_alphabet(void)
-int _islower(int c)
+void print_putchar(void);
+void print_print_alphabet_x10(void);
+void print_alphabet(void);
+int _islower(int c);
 
 #endif 
