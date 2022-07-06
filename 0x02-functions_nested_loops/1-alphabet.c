461a9a6e02b429e37bd5653a0e214d8b642d7353
Sunusi Sani Usman <sunusidogo@gmail.com>
2022-07-06 05:15:55 -0700

    jgjg

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * main - prints alphabets in lowercase
+ * main - prints alphabet in lowercase
  *
- * Return: alphabt
+ * Return: alphabet
  */
 void print_alphabet(void)
 {
 	char alphabet;
 
-	for (alphabet = 'a'; alphabet < 'z'; alphabet++)
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	_putchar(alphabet);
 	_putchar('\n');
 }
