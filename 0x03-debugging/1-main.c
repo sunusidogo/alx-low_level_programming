7b305746f5a7ca23f7fb0568b04dc3fe5d9d4758
Sunusi Sani Usman <sunusidogo@gmail.com>
2022-07-07 05:25:55 -0700

    updated

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * * main - causes an infinite loop
- * * Return: 0
- * */
+ * main - causes an infinite loop
+ * Return: 0
+ */
 
 int main(void)
 {
@@ -14,9 +14,9 @@ int main(void)
 	i = 0;
 	while (i < 10)
 	{
-		putchar(i);	
+		putchar(i);
 	}
-	/* don't delete it!*/
+	/*don't delete it!*/
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -4,13 +4,15 @@
 /* more headers goes there*/
 /* Betty style for function  goes there*/
 /**
- * main - print if number is postive, zero or negative
+ * positive_or_negative - print if number is postive, zero or negative
+ * @n: int n
  * Return: always 0
  */
-int positive_or_negative(void)
+int positive_or_negative(int n)
 {
 	int n;
-	srand (time(0));
+
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/*your code goes there*/
 	if (n > 0)
