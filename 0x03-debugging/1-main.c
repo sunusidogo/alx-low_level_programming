0cc1d06765d15803d77afe1c8a20bce17f3fafc6
Sunusi Sani Usman <sunusidogo@gmail.com>
2022-07-07 12:54:56 -0700

    gsdgdg

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,15 +9,14 @@ int main(void)
 {
 	int i;
 
-	/* don't delete it!*/
 	printf("Infinite loop incoming :(\n");
 
-	i = 0;
-	while (i < 10)
-	{
-		putchar(i);
-	}
-	/* don't delete it!*/
+	i  = 0;
+
+	/*while (i < 10)*/
+	/*{ */
+	/*putchar(i);*/
+	/*} */
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
