6de1c2cebaa47ef1956396131546f76ae9e0c59c
Sunusi Sani Usman <sunusidogo@gmail.com>
2022-07-08 09:07:09 -0700

    done

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -9,12 +9,13 @@
 int _isdigit(int c)
 {
 
-	if (c >= 0 || c <= 9)		
-	_putchar(1);
-
+	if (c >= 48 && c <= 57)
+	{
+	return (1);
+	}
 	else
-	_putchar(0);
-	_putchar('\n');
-
+	{
 	return (0);
+	}
+
 }
