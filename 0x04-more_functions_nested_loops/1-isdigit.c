6c944a6ba6d40226087f438460f15434c97d096f
Sunusi Sani Usman <sunusidogo@gmail.com>
2022-07-08 08:37:57 -0700

    hfgf

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit from 0 to 9
+ * @c: integer c
+ *
+ * Return: 0
+*/
+int _isdigit(int c)
+{
+
+	if (c >= 0 || c <= 9)		
+	_putchar(1);
+
+	else
+	_putchar(0);
+	_putchar('\n');
+
+	return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,7 +3,8 @@
 int _putchar(char c);
 int main(void);
 int _isupper(int c);
-
+int _isdigit(int c);
+int main(void);
 
 
 #endif
