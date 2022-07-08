ac775084a3a0f0cb96cf39bfe082d0a4456b5fc8
Sunusi Sani Usman <sunusidogo@gmail.com>
2022-07-08 07:05:01 -0700

    gdgd

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -9,13 +9,17 @@
 int _isupper(int c)
 {
 
-	if (c >= 65 && c <= 90)
-	_putchar(1);
+	
 
+	if (c >= 65 && c <= 90)
+	{
+	_putchar(1);
+	}
 	else
 	{
 	_putchar(0);
 	_putchar('\n');
 	}
-	return (0);
+
+	return(0);
 }
