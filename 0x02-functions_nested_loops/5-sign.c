978523ce70c38774bd2f1d824e74790343b0a930
Sunusi Sani Usman <sunusidogo@gmail.com>
2022-07-09 06:50:33 -0700

    added

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,20 +9,19 @@ int print_sign(int n)
 {
 	n = 0;
 
-	if (n > 1)
+	if (n > 0)
 {
-	return (1);
 	_putchar('+');
+	return (1);
 }
 	else if (n == 0)
 {
+	_putchar('0');
 	return (0);
-	_putchar(0);
 }
 	else
 {
-	return (-1);
 	_putchar('-');
+	return (-1);
 }
-	_putchar('\n');
 }
