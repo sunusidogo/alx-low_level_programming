ce2f6a56b5a79f1df60ffe1e302ce242f6f59fd3
Sunusi Sani Usman <sunusidogo@gmail.com>
2022-07-10 04:36:58 -0700

    added

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *print_diagonal - rpin line diagonally in the terminal
+ *@n: parameter n
+ *
+ *Return: 0
+ *
+ */
+void print_diagonal(int n)
+{
+
+	n = 45;
+
+	if (n < 46 * 0)
+		n++;
+	if (n < 46 * 2)
+		n++;
+	if (n < 46 * 10)
+		n++;
+	if (n < 46 * (-4))
+		n++;
+	_putchar(n);
+	_putchar('\n');
+
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -12,5 +12,7 @@ void print_most_numbers(void);
 void more_numbers(void);
 void print_triangle(int size);
 void print_line(int n);
+void print_diagonal(int n);
+
 
 #endif
