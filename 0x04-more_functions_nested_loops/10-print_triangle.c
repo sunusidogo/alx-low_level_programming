1e5818a135ee6e496432ae3e946a723d54f30c7c
Sunusi Sani Usman <sunusidogo@gmail.com>
2022-07-10 07:14:07 -0700

    added

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,24 +6,18 @@
  */
 void print_triangle(int size)
 {
-	int sp, ro, tr;
+	int i, j;
 
-	if (size <= 0)
+	for (i = 1; i <= size; i++)
 	{
+		for (j = 1; j <= i; j++)
+		{
+			if (size >= 1)
+				_putchar(35);
+			else
+				_putchar('\n');
+		}
+
 		_putchar('\n');
 	}
-	else
-	{
-	for (ro = 0; ro <= (size - 1); ro++)
-		
-	for (sp = 0; sp < (size - 1) - ro; sp++)
-	{
-		_putchar(' ');
-	}
-	for (tr = 0; tr <= ro; tr++)
-		
-		_putchar('#');
-	}
-		_putchar('\n');
-
 }
