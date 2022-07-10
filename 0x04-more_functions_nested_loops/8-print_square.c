7eff7cb90b0a6029574ff68ddf517e9767cdbf72
Sunusi Sani Usman <sunusidogo@gmail.com>
2022-07-10 06:55:03 -0700

    added

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,11 +11,16 @@ void print_square(int size)
 	int i;
 	int j;
 
-	for (i = 0; i <= size; i++)
+	for (i = 1; i <= size; i++)
 	{
-		for (j = 0; j <= size; j++)
+		for (j = 1; j <= size; j++)
 		{
+			if (size > 0)
 			_putchar(35);
+
+			else
+
+			_putchar('\n');
 		}
 		_putchar('\n');
 	}
