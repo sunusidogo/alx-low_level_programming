c3b5cae0a96da40d0a00d688493d27e10217bf7c
Sunusi Sani Usman <sunusidogo@gmail.com>
2022-07-10 06:41:26 -0700

    added

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_square - print square shape using # character.
+ * @size: is the size of the squre
+ *
+ * Return: 0
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= size; i++)
+	{	
+		for (j = 0; j <= size; j++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+
+}
