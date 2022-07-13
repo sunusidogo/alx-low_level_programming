5881164c162717c29ea7f381666082f4292231b0
Sunusi Sani Usman <sunusidogo@gmail.com>
2022-07-12 23:01:22 -0700

    0-98 updated

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,23 +1,14 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * reset_to_98 - takes a pointer to an int and updates its value to 98
+ * reset_to_98 - takes a pointer to and updates its value to 98
  * @n: a ponter to p.
  *
  * Return: 0
  */
 void reset_to_98(int *n)
 {
-	int p;
+	*n = 98;
 
-	*n = &p;
-	p = 402;
-
-	printf("value of 'p': %d\n", p);
-	printf("address of 'p': %p\n", &p);
-	printf("value of 'n': %p\n", n);
-	printf("address of 'p': %p\n", &n);
-	printf("value of 'p': %d\n", p);
-	return (0);
 
 }
--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,5 +1,20 @@
 #includ "main.h"
 
 /**
+ * _strlen -  function that returns the length of a string
+ * @s: string to pring
  *
- *
+ * Return: 0
+ */ 
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	_putchar(i);
+	return 0;
+	
+}
