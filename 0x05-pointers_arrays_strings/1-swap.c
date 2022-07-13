c12fea4944e7a38aa2ecf21d5ee009b77da86add
Sunusi Sani Usman <sunusidogo@gmail.com>
2022-07-12 23:15:55 -0700

    swap file added

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
-
 /**
  * swap_int - proghram to swap value between intergers
- * 
- *Return: 0
+ * @a: parameter swap
+ * @b: parameter to swap with
+ * Return: 0
  *
  */
 void swap_int(int *a, int *b)
 {
-	
+	int s = *a;
+	*a = *b;
+	*b = s;
 }
