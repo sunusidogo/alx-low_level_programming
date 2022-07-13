667be8854ef74f0cd7796f0d0a7287b1550d518a
Sunusi Sani Usman <sunusidogo@gmail.com>
2022-07-13 10:48:06 -0700

    added

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <string.h>
 /**
  * puts2 - program to print every other character in string
  * @str: string to print
@@ -8,5 +8,15 @@
  */
 void puts2(char *str)
 {
-	
+	int s;
+
+	for (int i = 0; i < strlen(str); i += 2)
+	{
+		_putchar(str[i]);
+
+		s[i / 2] = str[i];
+	}
+	_putchar(s);
+	_putchar('\n');
+	return (0);
 }
--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * puts_half - print half of a string
@@ -9,6 +10,13 @@
  */
 void puts_half(char *str)
 {
+	int i;
+	int part_size = *str / 2;
 
+	for (i = 0; i < *str; i++)
 
+	if (i % part_size == 0)
+	_putchar(str[i]);
+
+	_putchar('\n');
 }
