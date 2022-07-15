dcfc804fab4813b9673b641a9a6c0d66da3cf3ee
Sunusi Sani Usman <sunusidogo@gmail.com>
2022-07-15 10:35:16 -0700

    updated

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,18 +11,19 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i; 
+	int i, j;
 
 	i = 0;
+	j = 0;
 
-	while (dest[i])
-	{
+	while (dest[i] != '\0')
 		i++;
-	}
-	for (n = 0; src[n] < n; n++)
+	while (src[j] != '\0' && j < n)
 	{
-		dest[i] = src[n];
+		dest[i] = src[j];
 		i++;
+		j++;
 	}
+		dest[i] = '\0';
 	return (dest);
 }
