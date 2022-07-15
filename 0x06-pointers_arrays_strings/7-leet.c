3520606150973fef990a9264c5f559360f4e2930
Sunusi Sani Usman <sunusidogo@gmail.com>
2022-07-15 08:42:22 -0700

    updated

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,10 +5,8 @@
  * *leet - encodes a string in 1337
  * @s: string to be encoded
  *
- * Return: the resulting string;
- *
+ * Return: the resulting string
  */
-
  char *leet(char *s)
 {
 	int i, j;
