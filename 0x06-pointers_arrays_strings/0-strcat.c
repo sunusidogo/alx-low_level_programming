ae16de499308bf04e80572df121944226aff0587
Sunusi Sani Usman <sunusidogo@gmail.com>
2022-07-15 10:51:20 -0700

    updated

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,9 +10,20 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	while (*(++dest));
+	int i, j;
 
-	while (*dest++ = *src++);
+	i = 0;
+	j = 0;
 
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
