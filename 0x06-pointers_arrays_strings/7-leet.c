3b6c54768346fb23ff9ed2a28bba25e98373400a
Sunusi Sani Usman <sunusidogo@gmail.com>
2022-07-15 11:18:21 -0700

    updated

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * leet - encodes a string in 1337
@@ -15,13 +14,12 @@ char *leet(char *s)
 	char *b = "4433007711";
 
 	for (i = 0; s[i] != '\0'; i++)
+
+	for (j = 0; j < 10; j++)
 	{
-		for (j = 0; j < 10; j++)
-		{
-			if (s[i] == a[j])
-			{					
-				s[i] = b[j];
-			}
+		if (s[i] == a[j])
+		{					
+			s[i] = b[j];
 		}
 	}
 
