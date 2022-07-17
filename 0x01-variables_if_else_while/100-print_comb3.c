3051d3a489baf8c04d345bd51fd65ea214ba5ab9
Sunusi Sani Usman <sunusidogo@gmail.com>
2022-07-17 00:39:21 -0700

    corrected

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,20 +6,25 @@
  */
 int main(void)
 {
-	int num1;
-	int num2;
-{
-	for (num1 = 0; num1 < 9; num1++)
-	for (num2 = 1; num2 < 10; num2++)
-{
-	putchar((num1 % 10) + '0');
-	putchar((num2 % 10) + '0');
-	if (num1 == 8 && num2 == 9)
-	continue;
-	putchar(',');
-	putchar(' ');
-}
-}
+	int n, m;
+
+	for (n = 48; n <= 56; n++)
+	{
+	for (m = 49; m <= 57; m++)
+	{
+		if (m > n)
+		{
+			putchar(n);
+			putchar(m);
+			if (n != 56 || m != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	}
 	putchar('\n');
+
 	return (0);
 }
