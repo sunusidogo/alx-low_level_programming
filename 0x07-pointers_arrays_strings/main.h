e0a4f3bd2e25676b4bcccff8326e7f57bec419b5
Sunusi Sani Usman <sunusidogo@gmail.com>
2022-07-19 04:29:15 -0700

    Write a function that searches a string for any of a set of bytes.

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 void print_diagsums(int *a, int size);
 unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
 
 
 #endif
