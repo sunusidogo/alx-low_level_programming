9df813a9f82b9b8071773d281b63960304f81b3c
Sunusi Sani Usman <sunusidogo@gmail.com>
2022-07-26 14:06:42 -0700

    updated

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,5 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 int main(int argc __attribute__((unused)), char *argv[]);
+int main(int argc, char *argv[] __attribute__((unused)));
+int _putchar(char c);
+int main(int argc, char *argv[]);
+int _atoi(char *s);
 
 #endif
