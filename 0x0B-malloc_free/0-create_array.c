bc1bf056749347e52a98c2c0d7e938cd50d9fa8d
Sunusi Sani Usman <sunusidogo@gmail.com>
2022-07-27 13:47:47 -0700

    updated

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,7 +19,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-        p = (char *) malloc(sizeof(char) * size);
+	p = (char *) malloc(sizeof(char) * size);
 	if (p == NULL)
 		return (0);
 	while (i < size)
