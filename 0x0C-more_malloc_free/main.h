0f599266521f7524dae1af48b60d67d65c7b9122
Sunusi Sani Usman <sunusidogo@gmail.com>
2022-07-31 14:59:03 -0700

    checked with bc lib

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,11 +2,13 @@
 #define MAIN_H
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
-int main(void);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 char *_memset(char *s, char b, unsigned int n);
-void simple_print_buffer(char *buffer, unsigned int size);
 void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void simple_print_buffer(char *buffer, unsigned int size);
+int is_digit(char *s);
 
 
 #endif
